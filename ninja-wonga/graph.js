cf6c382015829f71541c3809c52e085973f648de
robertocannella <robertocannella@gmail.com>
2022-03-16 17:01:57 -0400

    added doughnut and paths

---
+++ ninja-wonga/graph.js
@@ -0,0 +1,60 @@
+const dims = { height: 300, width: 300, radius: 150 };
+const cent = { x: (dims.width / 2 + 5), y: (dims.height / 2 + 5) };
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', dims.width + 150)  // add space for legend
+    .attr('height', dims.height + 150); // add space for potential use
+
+const graph = svg.append('g')
+    .attr('transform', `translate(${cent.x},${cent.y})`)
+
+// pie generator
+const pie = d3.pie()
+    .sort(null)
+    .value(d => d.cost)
+
+// feed pie data into arc generator
+const arcPath = d3.arc()
+    .outerRadius(dims.radius) // radius of the pie chart
+    .innerRadius(dims.radius / 2)
+
+// UPDATE
+const update = (data) => {
+
+    // join enhance (pie) data to path elements
+    const paths = graph.selectAll('path')
+        .data(pie(data)) // pass data into pie generator
+
+    paths.enter()
+        .append('path')
+        .attr('class', 'arc')
+        .attr('d', arcPath) //  generate string as path 
+        .attr('stroke', '#FFF')
+        .attr('stroke-width', '3px')
+
+}
+
+// data array / firestore
+var data = []
+db.collection('expenses').onSnapshot(res => {
+
+    res.docChanges().forEach(change => {
+        const doc = { ...change.doc.data(), id: change.doc.id } // create new object with ID field from firestore
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc)
+                break;
+            case 'modified':
+                const index = data.findIndex((item) => item.id == doc.id) // get the item from data []
+                data[index] = doc; // overwrite old element with the modified one
+                break;
+            case 'removed':
+                data = data.filter((item) => item.id !== doc.id) // filter out the removed element as new array
+                break;
+            default: // default case required
+                break;
+        }
+    });
+    update(data);
+})
