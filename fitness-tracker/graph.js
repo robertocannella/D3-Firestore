9aff9d51c4f372a9f3a717b8fa11b08d265d6af8
robertocannella <robertocannella@gmail.com>
2022-03-17 13:48:12 -0400

    fitness tracker - added graph + svg attributes

--- fitness-tracker/graph.js
+++ fitness-tracker/graph.js
@@ -1,5 +1,20 @@
+// Margins + Dimensions
+const margin = { top: 40, right: 20, bottom: 50, left: 100 };
+
+// graph attributes (not svg)
+const graphWidth = 560 - margin.left - margin.right; // svg container width
+const graphHeight = 400 - margin.top - margin.bottom;
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', graphWidth + margin.left + margin.right)
+    .attr('height', graphHeight + margin.left + margin.right)
+const graph = svg.append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left},${margin.top})`)
+
 const update = (data) => {
-    console.log(data)
+    console.log(data);
 }
 
 // data and firestore
